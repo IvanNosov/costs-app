7d8f1b13fe55df32061d5b24dbd3a8a2ca995d08
Ivan Nosov <ivan7en@gmail.com>
2023-02-07 10:12:29 +0200

    Init project base

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import Costs from "./components/Costs";
 
 function App() {
+  const costs = [
+    {
+      date: new Date(2023, 2, 2),
+      description: "Fridge",
+      amount: 10,
+    },
+    {
+      date: new Date(2023, 2, 2),
+      description: "Fridge A12334",
+      amount: 10,
+    },
+    {
+      date: new Date(2023, 2, 2),
+      description: "Fridge A123",
+      amount: 1000,
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Costs costs={costs}/>
     </div>
   );
 }
